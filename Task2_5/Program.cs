2ab091fb3a21b97a659167b616206a443bb65990
Alexander Gokkanen <alex.gokkanen@gmail.com>
2025-02-16 20:43:31 +0300

    Task2_5

---
+++ Task2_5/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Task2_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the length of the pipe: ");
+            double length = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(Math.Ceiling(length));
+        }
+    }
+}
