338343ba273605d5e26104a75709127c5d72003e
Alexander Gokkanen <alex.gokkanen@gmail.com>
2025-02-16 19:57:16 +0300

    NewMessage

--- Task1_1/Program.cs
+++ Task1_1/Program.cs
@@ -1,10 +1,7 @@
-﻿namespace Task1_1
+﻿internal class Program
 {
-    internal class Program
+    private static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Console.WriteLine("Hello, World!");
     }
 }
---
+++ Task2_1/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+        int c = Convert.ToInt32(Console.ReadLine());
+        int d = a;
+        a = c;
+        c = b;
+        b = d;
+        Console.WriteLine("{0} \n{1} \n{2}", a, b, c);
+    }
+}
