ecf2fd4616df01d8381b1620bcb8b7dad2533fe9
Alexander Gokkanen <alex.gokkanen@gmail.com>
2025-02-16 21:01:13 +0300

    Task2_7

---
+++ Task2_6/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Task2_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the length of the pipe: ");
+            decimal length = Math.Round(Convert.ToDecimal(Console.ReadLine()));
+            Console.WriteLine(length);
+        }
+    }
+}
---
+++ Task2_7/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Task2_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two numbers: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0} \n{1}", b, a); //ну не сказано, что мы должны именно значения обменять:))
+            a = a + b;
+            b = a - b;
+            a = a - b;
+            Console.WriteLine("{0} \n{1}", a, b);
+        }
+    }
+}
