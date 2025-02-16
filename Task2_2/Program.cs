6a120d4da1e6f426fd30a60bbec6d47cb042d02d
Alexander Gokkanen <alex.gokkanen@gmail.com>
2025-02-16 20:11:30 +0300

    Task2_2

---
+++ Task2_2/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Enter the degrees: ");
+        int d = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter the minutes: ");
+        int m = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter the seconds: ");
+        int s = Convert.ToInt32(Console.ReadLine());
+        double r = Math.Round((d + m / 60 + s / 3600) * Math.PI / 180, 4);
+        Console.WriteLine(r);
+    }
+}
