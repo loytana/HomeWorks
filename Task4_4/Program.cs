239f2836d6af693462b5452e52468f9edf8fa51a
Alexander Gokkanen <alex.gokkanen@gmail.com>
2025-03-02 12:02:09 +0300

    Task4_4

---
+++ Task4_4/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task4_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+            double res = 1;
+            for (int i = 1; i <= b; i++)
+            {
+                res *= a;
+
+            }
+            Console.WriteLine(1/res);
+        }
+    }
+}
