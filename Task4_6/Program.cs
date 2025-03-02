9280b1994c9401fb749e877c027c2c09e042ab7b
Alexander Gokkanen <alex.gokkanen@gmail.com>
2025-03-02 15:58:09 +0300

    Task4_6

---
+++ Task4_6/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Task4_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            if (a == 1)
+            {
+                Console.WriteLine("Является");
+            }
+            else
+            {
+                do
+                {
+                    a %= 2;
+                } while (a >= 2);
+                if (a == 0)
+                {
+                    Console.WriteLine("Является");
+                }
+                else
+                {
+                    Console.WriteLine("Не является");
+                }
+            }
+        }
+    }
+}
