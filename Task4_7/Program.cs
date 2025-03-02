63fdfdaec82d421b6470cff6b932775254c112b4
Alexander Gokkanen <alex.gokkanen@gmail.com>
2025-03-02 23:13:28 +0300

    Task4_7

---
+++ Task4_7/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Task4_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+            if (a == 0)
+            {
+                Console.WriteLine("1");
+            }
+            else
+            {
+                while (a > 0)
+                {
+                    count++;
+                    a /= 10;
+                }
+                Console.WriteLine(count);
+            }
+        }
+    }
+}
