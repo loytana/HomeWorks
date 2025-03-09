04765568b715be3d6c37d0390cd68e57e4b81b42
Alexander Gokkanen <alex.gokkanen@gmail.com>
2025-03-09 15:04:19 +0300

    Task5_4

---
+++ Task5_4/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Task5_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] task = new int[10];
+            Random rnd = new Random();
+            for (int i = 0; i < task.Length; i++)
+            {
+                task[i] = rnd.Next(0, 11);
+                Console.Write("{0} ", task[i]); //проверка
+            }
+            Console.WriteLine();
+            for (int i = 0; i < task.Length / 2; i++)
+            {
+                int temp = task[i];
+                task[i] = task[task.Length - 1 - i];
+                task[task.Length - 1 - i] = temp;
+            }
+            foreach (int a in task) //проверка
+            {
+                Console.Write("{0} ", a);
+            }
+        }
+    }
+}
