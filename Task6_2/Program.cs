f360d98ab2aa387e4362e4a4debaf9c061de7a86
Alexander Gokkanen <alex.gokkanen@gmail.com>
2025-03-23 10:57:18 +0300

    Task6_2

--- Task6_2/Program.cs
+++ Task6_2/Program.cs
@@ -6,6 +6,12 @@
         {
             Console.WriteLine("Введите строку: ");
             string input = Console.ReadLine();
+            input = input.ToLower().Replace(" ", "");
+            string inputReversed = new string(input.Reverse().ToArray());
+            if (String.Compare(input, inputReversed) == 0)
+                Console.WriteLine("Палиндром");
+            else
+                Console.WriteLine("Не палиндром");
         }
     }
 }
