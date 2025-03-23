300062ab81f3d3a273d74fe9e75b05a971c28b5e
Alexander Gokkanen <alex.gokkanen@gmail.com>
2025-03-23 14:19:40 +0300

    Task6_5

---
+++ Task6_5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Task6_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите номер");
+            string number = Console.ReadLine();
+            Regex regex = new Regex(@"\+[0-9]\([0-9][0-9][0-9]\)[0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]");
+            if (regex.IsMatch(number))
+                Console.WriteLine("Является");
+            else 
+                Console.WriteLine("Не является");
+        }
+    }
+}
