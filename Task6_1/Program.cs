67400259cc7b24d0a19af1b2c94c039b5c87eb41
Alexander Gokkanen <alex.gokkanen@gmail.com>
2025-03-23 10:35:56 +0300

    Task6_1

---
+++ Task6_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+namespace Task6_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку: ");
+            string input = Console.ReadLine();
+            string[] words = input.Split();
+            int length = 0;
+            StringBuilder found = new StringBuilder(); //использую StringBuilder, чтобы уменьшить расход памяти
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length > length)
+                {
+                    found.Clear();
+                    length = words[i].Length; 
+                    found.Append(words[i]);
+                    found.Replace(",", "");
+                    found.Replace(":", "");
+                    found.Replace(";", "");
+                }
+            }
+            Console.WriteLine(found);
+        }
+    }
+}
