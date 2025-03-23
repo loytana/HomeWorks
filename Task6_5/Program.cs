3339a6179c654c021d2a1f16ef24aaddd386b211
Alexander Gokkanen <alex.gokkanen@gmail.com>
2025-03-23 14:41:10 +0300

    Task6_5

--- Task6_5/Program.cs
+++ Task6_5/Program.cs
@@ -8,7 +8,7 @@ namespace Task6_5
         {
             Console.WriteLine("Введите номер");
             string number = Console.ReadLine();
-            Regex regex = new Regex(@"\+[0-9]\([0-9][0-9][0-9]\)[0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]");
+            Regex regex = new Regex(@"\+[0-9]\([0-9]{3,}\)[0-9]{3,}-[0-9]{2,}-[0-9]{2,}");
             if (regex.IsMatch(number))
                 Console.WriteLine("Является");
             else 
