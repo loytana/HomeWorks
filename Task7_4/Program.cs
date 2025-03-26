517a54abe496c962e47fcf60cb59736d1844e24d
Alexander Gokkanen <alex.gokkanen@gmail.com>
2025-03-27 00:03:04 +0300

    Task7_4

---
+++ Task7_4/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task7_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxNumber = FindMax(10, 25, 5, 30, 15);
+            Console.WriteLine($"Max number: {maxNumber}");
+        }
+        static int FindMax(params int[] nums)
+        {
+            int max = 0;
+            foreach (int num in nums)
+                if (num > max)
+                    max = num;
+            return max;
+        }
+    }
+}
