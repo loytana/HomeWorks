1a343134b99a0cd8747e56fe7f31d96d7929dbd4
Alexander Gokkanen <alex.gokkanen@gmail.com>
2025-04-07 23:57:09 +0300

    Task9_1

---
+++ Task9_1/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Task9_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book("Мастер и Маргарита", "М. Булгаков", 1928, 160);
+            Console.WriteLine(book.GetInfo());
+            book.Year = 1929;
+            book.Pages = 180;
+            Console.WriteLine(book.GetInfo());
+        }
+    }
+    class Book
+    {
+        private string _title;
+        private string _author;
+        
+        public int Year { get; set; }
+        public int Pages { get; set; }
+
+        public Book(string title, string author, int year, int pages)
+        {
+            _title = title;
+            _author = author;
+            Year = year;
+            Pages = pages;
+        }
+        public string GetInfo()
+        {
+            return $"{_title}, {_author}, {Year}, {Pages} стр.";
+        }
+    }
+}
