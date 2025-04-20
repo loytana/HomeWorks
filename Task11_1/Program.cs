470026fdbadc5a2be056d63ea510537e41500aa7
Alexander Gokkanen <alex.gokkanen@gmail.com>
2025-04-20 22:28:45 +0300

    Task11_1

---
+++ Task11_1/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Task11_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal[] animals = new Animal[] {
+                new Dog(),
+                new Cat()
+            };
+            foreach (var animal in animals)
+            {
+                animal.ShowInfo();
+            }
+        }
+    }
+    public abstract class Animal
+    {
+        public abstract string Name { get; }
+        public abstract void Say();
+        public void ShowInfo()
+        {
+            Console.Write($"Название животного: {Name}\nЗвук:");
+            Say();
+        }
+    }
+    public class Dog : Animal
+    {
+        public override string Name => "Собака";
+        public override void Say()
+        {
+            Console.WriteLine("Гав!");
+        }
+    }
+    public class Cat : Animal
+    {
+        public override string Name => "Кошка";
+        public override void Say()
+        {
+            Console.WriteLine("Мяу!");
+        }
+    }
+}
