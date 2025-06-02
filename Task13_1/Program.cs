4df884669fc805dabeb0b2f60f47164a4d7a6173
Alexander Gokkanen <alex.gokkanen@gmail.com>
2025-06-02 20:01:38 +0300

    Task13_1

---
+++ Task13_1/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Task13_1
+{
+    internal class Program
+    {
+        public delegate int Transformer(int x);
+
+        static int[] Transform(int[] array, Transformer transformer)
+        {
+            int[] result = new int[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                result[i] = transformer(array[i]);
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            var numbers = new int[] { -2, 3, -5, 7, 10 };
+
+            //Удвоение всех чисел
+            Transformer doubleTransformer = x => x * 2;
+            var doubled = Transform(numbers, doubleTransformer);
+            foreach (int x in doubled)
+            
+                Console.Write($"{x} ");
+            
+            Console.WriteLine();
+
+            //Возведение в квадрат
+            Transformer squareTransformer = x => x * x;
+            var squared = Transform(numbers, squareTransformer);
+            Console.WriteLine("Квадраты чисел: " + string.Join(", ", squared));
+
+            //Замена чисел на их модули
+            Transformer absTransformer = Math.Abs;
+            var absolutes = Transform(numbers, absTransformer);
+            Console.WriteLine("Модули чисел: " + string.Join(", ", absolutes));
+        }
+    }
+}
